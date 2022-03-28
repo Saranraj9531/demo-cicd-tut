64744a33a006582148f709a6c163bed1a4d5a8ce
saranraj9531 <saranraj9531@gmail.com>
2022-03-28 19:58:01 +0530

    chnage in deployer

--- deploy.php
+++ deploy.php
@@ -55,7 +55,7 @@ task('deploy', [
     'deploy:vendors',
     // 'deploy:writable',
     'artisan:storage:link', // |
-    'artisan:view:cache',   // |
+    'artisan:view:clear',   // |
     'artisan:config:cache', // | Laravel specific steps
     'artisan:queue:restart', // | 
     'artisan:optimize',     // |
